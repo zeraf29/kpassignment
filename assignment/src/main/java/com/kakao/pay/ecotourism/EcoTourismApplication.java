1cd790cfc639db7d54d3824981f054e42f917fb9
jinhyupkim <jinhyupkim@218.38.137.27>
2019-03-25 22:43:46 +0900

    commit

--- assignment/src/main/java/com/kakao/pay/ecotourism/EcoTourismApplication.java
+++ assignment/src/main/java/com/kakao/pay/ecotourism/EcoTourismApplication.java
@@ -1,13 +1,13 @@
-package com.kakao.pay.assignment;
+package com.kakao.pay.ecotourism;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AssignmentApplication {
+public class EcoTourismApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AssignmentApplication.class, args);
+		SpringApplication.run(EcoTourismApplication.class, args);
 	}
 
 }
--- assignment/src/main/java/com/kakao/pay/ecotourism/ServletInitializer.java
+++ assignment/src/main/java/com/kakao/pay/ecotourism/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.kakao.pay.assignment;
+package com.kakao.pay.ecotourism;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(AssignmentApplication.class);
+		return application.sources(EcoTourismApplication.class);
 	}
 
 }
